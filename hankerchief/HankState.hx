dc33ccf442e54360430380d4b3f18e5979136795
Nat Quayle Nelson <natquaylenelson@gmail.com>
2019-05-14 16:48:02 -0600

    Fixing???

--- hankerchief/HankState.hx
+++ hankerchief/HankState.hx
@@ -1,12 +1,18 @@
 package hankerchief;
 
 import flixel.FlxState;
+import hank.Story;
 
+@:build(hank.FileLoadingMacro([AssetPaths.main__hank]))
 class HankState extends FlxState
 {
+    var story: Story;
+
 	override public function create():Void
 	{
 		super.create();
+
+        story = Story.FromFile();
 	}
 
 	override public function update(elapsed:Float):Void
