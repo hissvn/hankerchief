615aa58b5727b5a364e077dff7e843c1df3919c2
Nat Quayle Nelson <natquaylenelson@gmail.com>
2019-05-14 20:37:55 -0600

    Display first text from a Story

--- hankerchief/HankState.hx
+++ hankerchief/HankState.hx
@@ -1,22 +1,41 @@
 package hankerchief;
 
 import flixel.FlxState;
+
 import hank.Story;
 
-@:build(hank.FileLoadingMacro([AssetPaths.main__hank]))
+@:build(hank.FileLoadingMacro.build(["story/main.hank"]))
 class HankState extends FlxState
 {
     var story: Story;
+    var awaitingChoice: Bool;
 
 	override public function create():Void
 	{
 		super.create();
 
-        story = Story.FromFile();
+        trace(files["story/main.hank"]);
+
+        story = Story.FromFile("story/main.hank", files);
+        awaitingChoice = false;
 	}
 
 	override public function update(elapsed:Float):Void
 	{
 		super.update(elapsed);
+
+        switch (story.nextFrame()) {
+            case HasText(text):
+            trace(text);
+                var text = new flixel.text.FlxText(0, 0, 0, text, 64);
+                text.screenCenter();
+                add(text);
+            case HasChoices(choices):
+            trace(choices);
+                // TODO
+            case Finished:
+            trace('finished');
+                // TODO
+        }
 	}
 }
